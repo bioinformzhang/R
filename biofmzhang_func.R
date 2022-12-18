1aae96f7d666a5fc654a40820fe8dfa2440624e5
bioinformzhang <114212003+bioinformzhang@users.noreply.github.com>
2022-12-18 16:28:54 +0800

    Create biofmzhang_func.R

--- biofmzhang_func.R
+++ biofmzhang_func.R
@@ -10,27 +10,31 @@
 
 #将tibble转为数据框(默认前7行)
     headdf <- function(df, i=7, j=NULL){
-                if(is(df, "matrix") | is(df, "data.frame") | is(df, "tibble")){
-                    if(is.null(j)){
-                        output <- df %>% head(i) %>% as.data.frame
+                    if(is(df, "matrix") | is(df, "data.frame") | is(df, "tibble")){
+                        if(is.null(j)){
+                            output <- df %>% head(i) %>% as.data.frame
+                        }else{
+                            output <- df[1:i, 1:j] %>% as.data.frame
+                        }
+                        return(output)
                     }else{
-                        output <- df[1:i, 1:j] %>% as.data.frame
+                        stop(" Please input the matrix/data.frame/tibble")
                     }
-                    return(output)
-                }else{
-                    stop(" Please input the matrix/data.frame/tibble")
                 }
-            }
 
 
 #nrow/ncol/colnames/的向量化
     vnrow <- Vectorize(nrow)
     vncol <- Vectorize(ncol)
-    vcolnames <- Vectorize(colnames)
     vdim <- function(data=data){lapply(data, dim)}
     vlength <- function(data=data){lapply(data, length)}
     vhead <- function(data=data){lapply(data, head)}
+    vcolnames <- Vectorize(colnames)
+    vrownames <- Vectorize(rownames)
+    vnames <- Vectorize(names)
 
 
-#alias View
-    v <- View
+
+
+
+
